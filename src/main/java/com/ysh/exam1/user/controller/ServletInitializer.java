8d87dc9960e1f0c072b88a37cb3b3a580a3c058e
seonghuncode <gnos456@naver.com>
2022-03-20 17:01:50 +0900

    폴더모듈화

--- src/main/java/com/ysh/exam1/user/controller/ServletInitializer.java
+++ src/main/java/com/ysh/exam1/user/controller/ServletInitializer.java
@@ -1,5 +1,6 @@
-package com.ysh.exam1;
+package com.ysh.exam1.user.controller;
 
+import com.ysh.exam1.Exam1Application;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
